427ba38e23d12541a687d8e8b955d2157a779125
Matt <mgould@breakingfreegroup.com>
2020-05-31 09:54:54 +0100

    data

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,8 @@
+import {default as Aug18} from './Aug18';
+import {default as Sep18} from './Sep18';
+import {default as Oct18} from './Oct18';
+import {default as Nov18} from './Nov18';
+import {default as Dec18} from './Dec18';
 import {default as Jan19} from './Jan19';
 import {default as Feb19} from './Feb19';
 import {default as Mar19} from './Mar19';
@@ -14,9 +19,15 @@ import {default as Jan20} from './Jan20';
 import {default as Feb20} from './Feb20';
 import {default as Mar20} from './Mar20';
 import {default as Apr20} from './Apr20';
+import {default as May20} from './May20';
 
 
 export default {
+  'Aug 18': Aug18,
+  'Sep 18': Sep18,
+  'Oct 18': Oct18,
+  'Nov 18': Nov18,
+  'Dec 18': Dec18,
   'Jan 19': Jan19,
   'Feb 19': Feb19,
   'Mar 19': Mar19,
@@ -32,5 +43,6 @@ export default {
   'Jan 20': Jan20,
   'Feb 20': Feb20,
   'Mar 20': Mar20,
-  'Apr 20': Apr20
+  'Apr 20': Apr20,
+  'May 20': May20
 }
