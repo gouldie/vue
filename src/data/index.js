1c6017ae1fd43d7597aca2605d81cec325c97a9d
Matt <mgould@breakingfreegroup.com>
2020-05-31 16:54:24 +0100

    2017 data

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
+import { Jan17, Feb17, Mar17, Apr17, May17, Jun17, Jul17, Aug17, Sep17, Oct17, Nov17, Dec17 } from './2017'
 import { Jan18, Feb18, Mar18, Apr18, May18, Jun18, Jul18, Aug18, Sep18, Oct18, Nov18, Dec18 } from './2018'
 import { Jan19, Feb19, Mar19, Apr19, May19, Jun19, Jul19, Aug19, Sep19, Oct19, Nov19, Dec19 } from './2019'
 import { Jan20, Feb20, Mar20, Apr20, May20 } from './2020'
@@ -5,6 +6,18 @@ import { Jan20, Feb20, Mar20, Apr20, May20 } from './2020'
 
 
 export default {
+  'Jan 17': Jan17,
+  'Feb 17': Feb17,
+  'Mar 17': Mar17,
+  'Apr 17': Apr17,
+  'May 17': May17,
+  'Jun 17': Jun17,
+  'Jul 17': Jul17,
+  'Aug 17': Aug17,
+  'Sep 17': Sep17,
+  'Oct 17': Oct17,
+  'Nov 17': Nov17,
+  'Dec 17': Dec17,
   'Jan 18': Jan18,
   'Feb 18': Feb18,
   'Mar 18': Mar18,
